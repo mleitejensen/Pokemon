3bc957fe8fed0f75a3df0a16adfb45dd4b27855e
mleitejensen <mleitejensen@gmail.com>
2024-10-16 21:09:05 +0200

    added tooltip to map areas

--- Pokemon/src/Pages/Kanto.tsx
+++ Pokemon/src/Pages/Kanto.tsx
@@ -1,3 +1,5 @@
+import Tooltip from "@mui/material/Tooltip";
+
 const Kanto = () => {
   return (
     <>
@@ -7,47 +9,129 @@ const Kanto = () => {
           src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3bddf750-53a0-4a9f-872f-8d13685a758f/d3c4hsg-5acbd78f-c4cb-4f40-a87a-05700ac859a4.png/v1/fill/w_900,h_883,q_80,strp/labeled_map_of_kanto_by_rythos_d3c4hsg-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODgzIiwicGF0aCI6IlwvZlwvM2JkZGY3NTAtNTNhMC00YTlmLTg3MmYtOGQxMzY4NWE3NThmXC9kM2M0aHNnLTVhY2JkNzhmLWM0Y2ItNGY0MC1hODdhLTA1NzAwYWM4NTlhNC5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.VhSNiL_oKJgugWzSc1MX-qJ2zPGcgwOHyEzjI9LlM-E"
           alt=""
         ></img>
-        <div className="indigoPlateau area"></div>
-        <div className="victoryRoad area"></div>
-        <div className="route23 area"></div>
-        <div className="route22 area"></div>
-        <div className="viridianCity area"></div>
-        <div className="route1 area"></div>
-        <div className="palletTown area"></div>
-        <div className="route21 area"></div>
-        <div className="route2 area"></div>
-        <div className="viridianForest area"></div>
-        <div className="pewterCity area"></div>
-        <div className="route3 area"></div>
-        <div className="mountainMoon area"></div>
-        <div className="route4 area"></div>
-        <div className="ceruleanCity area"></div>
-        <div className="route24 area"></div>
-        <div className="route25 area"></div>
-        <div className="route5 area"></div>
-        <div className="route6 area"></div>
-        <div className="vermilionCity area"></div>
-        <div className="route7 area"></div>
-        <div className="route11 area"></div>
-        <div className="route12 area"></div>
-        <div className="lavenderTown area"></div>
-        <div className="rockTunnel area"></div>
-        <div className="powerPlant area"></div>
-        <div className="route9 area"></div>
-        <div className="celadonCity area"></div>
-        <div className="route8 area"></div>
-        <div className="route16 area"></div>
-        <div className="cyclingRoad area"></div>
-        <div className="route18 area"></div>
-        <div className="safariZone area"></div>
-        <div className="fuchsiaCity area"></div>
-        <div className="route13 area"></div>
-        <div className="route14 area"></div>
-        <div className="route15 area"></div>
-        <div className="route19 area"></div>
-        <div className="seafoamIslands area"></div>
-        <div className="route20 area"></div>
-        <div className="cinnabarIsland area"></div>
+        <Tooltip title="Indigo Plateau">
+          <div className="indigoPlateau area"></div>
+        </Tooltip>
+        <Tooltip title="Victory Road">
+          <div className="victoryRoad area"></div>
+        </Tooltip>
+        <Tooltip title="Route 23">
+          <div className="route23 area"></div>
+        </Tooltip>
+        <Tooltip title="Route 22">
+          <div className="route22 area"></div>
+        </Tooltip>
+        <Tooltip title="Viridian City">
+          <div className="viridianCity area"></div>
+        </Tooltip>
+        <Tooltip title="Route 1">
+          <div className="route1 area"></div>
+        </Tooltip>
+        <Tooltip title="Pallet Town">
+          <div className="palletTown area"></div>
+        </Tooltip>
+        <Tooltip title="Route 21">
+          <div className="route21 area"></div>
+        </Tooltip>
+        <Tooltip title="Route 2">
+          <div className="route2 area"></div>
+        </Tooltip>
+        <Tooltip title="Viridian Forest">
+          <div className="viridianForest area"></div>
+        </Tooltip>
+        <Tooltip title="Pewter City">
+          <div className="pewterCity area"></div>
+        </Tooltip>
+        <Tooltip title="Route 3">
+          <div className="route3 area"></div>
+        </Tooltip>
+        <Tooltip title="Mount Moon">
+          <div className="mountainMoon area"></div>
+        </Tooltip>
+        <Tooltip title="Route 4">
+          <div className="route4 area"></div>
+        </Tooltip>
+        <Tooltip title="Cerulean City">
+          <div className="ceruleanCity area"></div>
+        </Tooltip>
+        <Tooltip title="Route 24">
+          <div className="route24 area"></div>
+        </Tooltip>
+        <Tooltip title="Route 25">
+          <div className="route25 area"></div>
+        </Tooltip>
+        <Tooltip title="Route 5">
+          <div className="route5 area"></div>
+        </Tooltip>
+        <Tooltip title="Route 6">
+          <div className="route6 area"></div>
+        </Tooltip>
+        <Tooltip title="Vermilion City">
+          <div className="vermilionCity area"></div>
+        </Tooltip>
+        <Tooltip title="Route 7">
+          <div className="route7 area"></div>
+        </Tooltip>
+        <Tooltip title="Route 11">
+          <div className="route11 area"></div>
+        </Tooltip>
+        <Tooltip title="Route 12">
+          <div className="route12 area"></div>
+        </Tooltip>
+        <Tooltip title="Lavender Town">
+          <div className="lavenderTown area"></div>
+        </Tooltip>
+        <Tooltip title="Rock Tunnel">
+          <div className="rockTunnel area"></div>
+        </Tooltip>
+        <Tooltip title="Power Plant">
+          <div className="powerPlant area"></div>
+        </Tooltip>
+        <Tooltip title="Route 9">
+          <div className="route9 area"></div>
+        </Tooltip>
+        <Tooltip title="Celadon City">
+          <div className="celadonCity area"></div>
+        </Tooltip>
+        <Tooltip title="Route 8">
+          <div className="route8 area"></div>
+        </Tooltip>
+        <Tooltip title="Route 16">
+          <div className="route16 area"></div>
+        </Tooltip>
+        <Tooltip title="Cycling Road">
+          <div className="cyclingRoad area"></div>
+        </Tooltip>
+        <Tooltip title="Route 18">
+          <div className="route18 area"></div>
+        </Tooltip>
+        <Tooltip title="Safari Zone">
+          <div className="safariZone area"></div>
+        </Tooltip>
+        <Tooltip title="Fuchsia City">
+          <div className="fuchsiaCity area"></div>
+        </Tooltip>
+        <Tooltip title="Route 13">
+          <div className="route13 area"></div>
+        </Tooltip>
+        <Tooltip title="Route 14">
+          <div className="route14 area"></div>
+        </Tooltip>
+        <Tooltip title="Route 15">
+          <div className="route15 area"></div>
+        </Tooltip>
+        <Tooltip title="Route 19">
+          <div className="route19 area"></div>
+        </Tooltip>
+        <Tooltip title="Seafoam Islands">
+          <div className="seafoamIslands area"></div>
+        </Tooltip>
+        <Tooltip title="Route 20">
+          <div className="route20 area"></div>
+        </Tooltip>
+        <Tooltip title="Cinnabar Island">
+          <div className="cinnabarIsland area"></div>
+        </Tooltip>
       </div>
     </>
   );
