89ef991ba2079a5ce069da345dd7adb749db124b
Martin Leite Jensen <martin.leite.jensen@nhn.no>
2024-10-16 14:36:25 +0200

    added some map areas

--- Pokemon/src/Pages/Kanto.tsx
+++ Pokemon/src/Pages/Kanto.tsx
@@ -2,6 +2,48 @@ const Kanto = () => {
   return (
     <>
       <h1>Kanto</h1>
+      <div className="imageDiv">
+        <img
+          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3bddf750-53a0-4a9f-872f-8d13685a758f/d3c4hsg-5acbd78f-c4cb-4f40-a87a-05700ac859a4.png/v1/fill/w_900,h_883,q_80,strp/labeled_map_of_kanto_by_rythos_d3c4hsg-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODgzIiwicGF0aCI6IlwvZlwvM2JkZGY3NTAtNTNhMC00YTlmLTg3MmYtOGQxMzY4NWE3NThmXC9kM2M0aHNnLTVhY2JkNzhmLWM0Y2ItNGY0MC1hODdhLTA1NzAwYWM4NTlhNC5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.VhSNiL_oKJgugWzSc1MX-qJ2zPGcgwOHyEzjI9LlM-E"
+          alt=""
+        ></img>
+        <div className="indigoPlateau"></div>
+        <div className="victoryRoad"></div>
+        <div className="route23"></div>
+        <div className="route22"></div>
+        <div className="viridianCity"></div>
+        <div className="route1"></div>
+        <div className="palletTown"></div>
+        <div className="route21"></div>
+        <div className="route2"></div>
+        <div className="viridianForest"></div>
+        <div className="pewterCity"></div>
+        <div className="route3"></div>
+        <div className="mountainMoon"></div>
+        <div className="route4"></div>
+        <div className="ceruleanCity"></div>
+        <div className="route24"></div>
+        <div className="route25"></div>
+        <div className="route5"></div>
+        <div className="route6"></div>
+        <div className="vermilionCity"></div>
+        <div className="route7"></div>
+        <div className="route11"></div>
+        <div className="route12"></div>
+        <div className="lavendertown"></div>
+        <div className="rockTunnel"></div>
+        <div className="powerPlant"></div>
+        <div className="route9"></div>
+        <div className="celadonCity"></div>
+        <div className="route16"></div>
+        <div className="cyclingRoad"></div>
+        <div className="route18"></div>
+        <div className="safariZone"></div>
+        <div className="fuchsiaCity"></div>
+        <div className="route13"></div>
+        <div className="route14"></div>
+        <div className="route15"></div>
+      </div>
     </>
   );
 };
