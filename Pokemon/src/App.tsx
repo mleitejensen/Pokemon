82276c06ff8abd15cb3fc845174e023c106a9d60
mleitejensen <mleitejensen@gmail.com>
2024-10-19 12:42:02 +0200

    removed useless stuff

--- Pokemon/src/App.tsx
+++ Pokemon/src/App.tsx
@@ -13,18 +13,6 @@ import Pokemon from "./Pages/Pokemon";
 
 const darkTheme = createTheme({
   palette: {
-    primary: {
-      50: "#C0CCD9",
-      100: "#A5B8CF",
-      200: "#6A96CA",
-      300: "#4886D0",
-      400: "#2178DD",
-      500: "#096BDE",
-      600: "#1B62B5",
-      700: "#265995",
-      800: "#2F4968",
-      900: "#2F3C4C",
-    },
     mode: "dark",
   },
 });
